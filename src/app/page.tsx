7316405b652360270cc0639303f3f6ad72894da7
Gustavo Miguelote <gustavomiguelote@me.com>
2023-07-19 10:45:26 +0100

    refactor(main): removing main component and adding context to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,49 @@
-'use client'
-import NavBar from './components/Navbar';
-import Main from './components/Main';
+"use client";
+import { ReactNode, useContext } from "react";
+import { AppContext } from "@/app/contexts/app.context";
+
+import NavBar from "./components/Navbar";
+import ListCountryItem from "./components/ListCountryItem";
+
+import { IAppContext } from "@/app/interfaces/IAppContext";
+import { ICountry } from "@/app/interfaces/ICountry";
 
 export default function Home() {
+  const contextValues = useContext<IAppContext>(AppContext);
+
+  const generateCountryListWithContextValues = (
+    contextValues: IAppContext,
+  ): ReactNode => {
+    if (contextValues.countriesFiltered.length > 0) {
+      return (
+        <ul>
+          {contextValues.countriesFiltered.map((country: ICountry) => (
+            <ListCountryItem key={country.cca2} country={country} />
+          ))}
+        </ul>
+      );
+    }
+
+    if (
+      contextValues.countriesFiltered.length === 0 &&
+      contextValues.searchTextContext !== ""
+    ) {
+      return <h2>404</h2>;
+    }
+
+    return (
+      <ul>
+        {contextValues.countriesInitial.map((country: ICountry) => (
+          <ListCountryItem key={country.cca2} country={country} />
+        ))}
+      </ul>
+    );
+  };
+
   return (
     <>
-      <NavBar/>
-      <Main />
+      <NavBar />
+      <main>{generateCountryListWithContextValues(contextValues)}</main>
     </>
-  )
+  );
 }
